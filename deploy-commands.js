6a43808495a52a97e2a607764d5d3afd6a2745d0
blastcraftmc <blastcraftmcbtw@gmail.com>
2022-12-11 13:39:51 -0500

    Gitignore

---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('4k').setDescription("Snaps a picture"),
+	new SlashCommandBuilder().setName('server').setDescription("Replies with server info!"),
+	new SlashCommandBuilder().setName('user').setDescription("Replies with user info!"),
+	new SlashCommandBuilder().setName('help').setDescription("Need some help?"),
+	new SlashCommandBuilder().setName('mute').setDescription("Mute user").addStringOption(option => option.setName('user').setDescription('User to mute').setRequired(true)),
+	new SlashCommandBuilder().setName('activity').setDescription("Adds Blastbot's activity").addStringOption(option => option.setName('activityoption').setDescription('Activity').setRequired(true)),
+	new SlashCommandBuilder().setName('apply').setDescription("Apply to help develop Blastbot"),
+	new SlashCommandBuilder().setName('ping').setDescription("See Blastbot's ping"),
+	new SlashCommandBuilder().setName('say').setDescription("Says message back to you").addStringOption(option => option.setName('message').setDescription('Message').setRequired(true)).addStringOption(option => option.setName('color').setDescription('Embed Color').setRequired(false)),
+	new SlashCommandBuilder().setName('join').setDescription("Apply to join Blastcraft")
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+//node deploy-commands.js
---
+++ index.js
@@ -0,0 +1,71 @@
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageSelectMenu, Modal, TextInputComponent, Message } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+	client.channels.cache.get('992810093332676629').send(`Hello there! I'm back up from my grave. Logged in as ${client.user.tag}`)
+	client.user.setActivity('Blastcraft');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === '4k') {
+		await interaction.reply({ content: `📸Screenshotted and saved to cloud`});
+	} else if (commandName === 'server') {
+		await interaction.reply({ content: `Server Name: ${interaction.guild.name}\nTotal Members: ${interaction.guild.memberCount}\nDate Created: ${interaction.guild.createdAt}`, ephemeral: true });
+	} else if (commandName === 'user') {
+		await interaction.reply({ content: `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`, ephemeral: true });
+	} else if (commandName === 'help') {
+		const helpEmbed = new MessageEmbed().setColor(`4B0082`).setTitle(`Need Help?`).addFields({ name: 'Slash Commands', value: 'All commands start with /' }, { name: '/ping', value: 'Use /ping to see the bots ping', inline: true }, { name: '/user', value: 'Use /user to get your user info', inline: true }, { name: '/server', value: 'Use /server to get the server info', inline: true }).setURL('http://blastcraft.rf.gd/blastbot');
+		await interaction.reply({ embeds: [helpEmbed], ephemeral: true})
+	} else if (commandName === 'mute') {
+		const target = interaction.options.getString('user');
+		if (member.roles.cache.has('819014466065924146')) {
+			await interaction.reply({ content: `Muted ${user}`, ephemeral: true})
+			const role = interaction.options.getRole('MUTED');
+			const member = interaction.options.getMember('user');
+			member.roles.add(role);
+		} else {
+			await interaction.reply({ content: `You don't have the nessesary permissions`, ephemeral: true})
+		}
+	} else if (commandName === 'activity') {
+		const target = interaction.options.getString('activityoption');
+		await interaction.reply({ content: `Setting status ${target}`, ephemeral: true})
+		client.user.setActivity(target);
+	} else if (commandName === 'apply') {
+		client.users.send('685599175735378140', `The user "${interaction.user.username}" (${interaction.user.id}) sent a Blastbot developer application`);
+		await interaction.reply({ content: `Applied successfully, await a response in less then a day`, ephemeral: true });
+	} else if (commandName === 'ping') {
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		interaction.editReply(`Websocket heartbeat: ${client.ws.ping}ms\nRoundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+	} else if (commandName === 'say') {
+		const target = interaction.options.getString('message');
+		const color = interaction.options.getString('color');
+		const sayEmbed = new MessageEmbed().setColor(color).setTitle(`${interaction.user.username} said: ${target}`);
+		await interaction.reply({ embeds: [sayEmbed] })
+	} else if (commandName === 'join') {
+		const modal = new Modal()
+        .setCustomId('name')
+        .setTitle('What is your name?')
+        .addComponents([
+        new MessageActionRow().addComponents(
+            new TextInputComponent()
+            .setCustomId('nameBox')
+            .setLabel('Answer')
+            .setStyle('SHORT')
+            .setMinLength(3)
+            .setMaxLength(15)
+            .setPlaceholder('Name')
+            .setRequired(true),
+        	)
+        ]);
+		await interaction.showModal(modal);
+	}
+});
+
+client.login(token);
